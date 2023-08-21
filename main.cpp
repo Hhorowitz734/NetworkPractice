fed24d45aeae838e8bd21639c1a52f5869deeded
Benjamin_Horowitz <hhorowitz734@gmail.com>
2023-08-20 21:43:21 -0500

    continued to main loop

--- main.cpp
+++ main.cpp
@@ -23,10 +23,51 @@ int main(){
         .sin_port = htons(PORT)
     };
 
+    //Creates socket
     int option = 1;
     int srv_addr_size = sizeof(saddr); //Server address size
     int socketServer = socket(AF_INET, SOCK_STREAM, 0);
     setsockopt(socketServer, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &option, sizeof(option));
+
+    if (socketServer == -1){ //Handles case that socket is not created
+        std::cerr << "Not able to create socket." << std::endl;
+        return 0;
+    }
+
+    //Client setup
+    struct sockaddr_in caddr;
+    socklen_t caddrSize = sizeof(caddr);
+    int socketClient;
+
+    //Binding our socket to the ip+port
+    bind(socketServer, (struct sockaddr*)&saddr, srv_addr_size);
+
+
+    //Listening
+    listen(socketServer, SOMAXCONN); //Max available connections that socket gets (128)
+    std::stringstream ss;
+    ss << PORT;
+    std::cout << "[SERVER]: Now listening on port" << ss.str() << ".\n";
+
+    char buff[4096];
+    int recievedDataSize; //Size of recieved data in bytes
+
+    //Main loop for waiting for clients
+    while (true){
+
+        //Accept any connections from clients here
+        socketClient = accept(socketServer, (struct sockaddr*)&caddr, (socklen_t*)&caddr);
+        std::cout << "[SERVER]: Client connected successfully.\n";
+
+        //Identifying the client
+        char hostClient[NI_MAXHOST];
+        char portClient[NI_MAXSERV];
+        memset(hostClient, 0, NI_MAXHOST);
+
+
+    }
+
+
 
     return 0;
 
