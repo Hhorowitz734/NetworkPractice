a3b8478839cb54d4d5f0c615f52a2ae171a19cce
Benjamin_Horowitz <hhorowitz734@gmail.com>
2023-08-21 10:43:19 -0500

    completed tutorial code but still some problems, saving

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 //Goal -> I will follow the following tutorial
 //https://www.youtube.com/watch?v=F3iIGUiW27Q&t=2141s
 //and try to recreate it while writing comments, and then use the code to playground socket stuff.
+//Current timestamp: 26:12
 
 int main(){
 
@@ -44,10 +45,14 @@ int main(){
 
 
     //Listening
-    listen(socketServer, SOMAXCONN); //Max available connections that socket gets (128)
+    int testListen = listen(socketServer, SOMAXCONN); //Max available connections that socket gets (128)
     std::stringstream ss;
     ss << PORT;
-    std::cout << "[SERVER]: Now listening on port" << ss.str() << ".\n";
+    if (testListen != -1){
+        std::cout << "[SERVER]: Now listening on Port " << ss.str() << ".\n";
+    } else {
+        std::cerr << "[SERVER]: Error listening on Port " << ss.str() << ".\n";
+    }
 
     char buff[4096];
     int recievedDataSize; //Size of recieved data in bytes
@@ -63,6 +68,33 @@ int main(){
         char hostClient[NI_MAXHOST];
         char portClient[NI_MAXSERV];
         memset(hostClient, 0, NI_MAXHOST);
+        memset(portClient, 0, NI_MAXSERV);
+
+        //Confirms we have a client
+        if (getnameinfo((sockaddr*)&caddr, sizeof(caddr), hostClient, NI_MAXHOST, portClient, NI_MAXSERV, 0) == 0){
+            //NOTE -> in the above line in "(sockaddr*)&caddr", this is known as a c-style cast
+            //We are just converting caddr's memory address to the sockaddr* datatype.
+            std::cout << "[SERVER]: " << hostClient << " is now connected to port " << portClient << '\n';
+        } else {
+            inet_ntop(AF_INET, &caddr.sin_addr, hostClient, NI_MAXHOST);
+            std::cout << "[SERVER]: " << hostClient << " is now connected to port " << ntohs(caddr.sin_port) << '\n';
+        }
+
+        //Recieve data
+        recievedDataSize = recv(socketClient, buff, 4096, 0);
+        if (recievedDataSize == -1){ //Case that data is not properly recieved
+            std::cerr << "[SERVER]: Error recieving data.\n";
+            break;
+        } else if (recievedDataSize == 0){ //If it's 0, the client disconnected
+            std::cout << "[SERVER]: Client disconnected.\n";
+            break;
+        }
+        send(socketClient, buff, recievedDataSize + 1, 0);
+
+        std::cout << std::string(buff, 0, recievedDataSize) << '\n';
+
+        close(socketClient);
+
 
 
     }
